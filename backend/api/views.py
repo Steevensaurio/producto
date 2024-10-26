6b173ca0acac5e9c023840b36a87e7f01d19c59a
Steeven Maila <steevenandresmaila@gmail.com>
2024-10-25 23:31:53 -0500

    Subida del backend personalizado con nuevos campos

---
+++ backend/api/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from userauth.models import User, Profile
+from api import serializer as api_serializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+
+class  MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = api_serializer.MyTokenObtainPairSerializer
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = [AllowAny]
+    serializer_class = api_serializer.RegisterSerializer
